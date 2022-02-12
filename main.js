898c1324df253b54005d7191347d6f3b4c5a284c
Vyom-Code <82333325+Vyom-Code@users.noreply.github.com>
2022-02-12 10:46:54 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(600,600);
+    canvas.position(950,300);
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('Vyom.png')
+}
